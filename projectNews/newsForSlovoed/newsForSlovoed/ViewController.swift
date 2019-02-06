f04f0969d38cb3d0d7fd64626a119d1daf5f0759
anton <nochnoy23>
2019-02-06 20:01:19 +0300

    Start project

---
+++ projectNews/newsForSlovoed/newsForSlovoed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsForSlovoed
+//
+//  Created by Антон on 06/02/2019.
+//  Copyright © 2019 Антон. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
