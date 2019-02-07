4fce9e78037ab9e7c7d772eea445400b4cb96333
Nochnoy <anton.nochnoy@paragon-software.com>
2019-02-07 12:17:45 +0300

    Add without save

---
+++ newsWithoutSaveData/newsWithOutSaveData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsWithoutSaveData
+//
+//  Created by Nochnoy on 07/02/2019.
+//  Copyright © 2019 Nochnoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
